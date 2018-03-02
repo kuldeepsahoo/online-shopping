0bdf1fc65a11d999ca5d28dd96d9da8f1ee9783b
kuldeep sahoo <kuldeepsahoo@gmail.com>
2018-03-02 12:13:44 +0530

    Set up completed

---
+++ shoppingbackend/src/main/java/net/kul/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.kul.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
